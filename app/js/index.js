90541fcafad608f640642c88529d57e0063fef1b
Rory Killick <rorykillick@gmail.com>
2021-09-11 23:50:19 +0200

    setInterval added

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,15 @@
-let now = new Date().getTime();
-const DeadlineDate = new Date ('September 25, 2021 22:00:00').getTime();
+let timer = setInterval(calculateTimeRemaining = () => {
+    let now = new Date().getTime();
+    const deadlineDate = new Date ('September 25, 2021 22:00:00').getTime();
+    
+    let time = deadlineDate - now
+    let days = Math.floor(time / (1000*60*60*24));
+    let hours = Math.floor((time % (1000*60*60*24))/(1000*60*60));
+    let minutes = Math.floor((time % (1000*60*60))/(1000*60));
+    let seconds = Math.floor((time % (1000*60))/1000);
 
-let time = DeadlineDate - now
-let days = Math.floor(time / (1000*60*60*24));
-let hours = Math.floor((time % (1000*60*60*24))/(1000*60*60));
-let minutes = Math.floor((time % (1000*60*60))/(1000*60));
-let seconds = Math.floor((time % (1000*60))/1000);
-
-document.querySelector('.days').innerHTML = days;
-document.querySelector('.hours').innerHTML = hours;
-document.querySelector('.minutes').innerHTML = minutes;
-document.querySelector('.seconds').innerHTML = seconds;
+    document.querySelector('.days').textContent = days;
+    document.querySelector('.hours').textContent = hours;
+    document.querySelector('.minutes').textContent = minutes;
+    document.querySelector('.seconds').textContent = seconds;
+}, 1000);
