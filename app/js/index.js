183849bb6c0c43c967aaca73aa2b93aaf20a1aba
Rory Killick <rorykillick@gmail.com>
2021-09-11 23:09:46 +0200

    added Math formulas for countdown

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+let now = new Date().getTime();
+const DeadlineDate = new Date ('September 25, 2021 22:00:00').getTime();
+
+let time = DeadlineDate - now
+let days = Math.floor(time / (1000*60*60*24));
+let hours = Math.floor((time % (1000*60*60*24))/(1000*60*60));
+let minutes = Math.floor((time % (1000*60*60))/(1000*60));
+let seconds = Math.floor((time % (1000*60))/1000);
+
+document.querySelector('.days').innerHTML = days;
+document.querySelector('.hours').innerHTML = hours;
+document.querySelector('.minutes').innerHTML = minutes;
+document.querySelector('.seconds').innerHTML = seconds;
